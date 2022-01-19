835376eb5e3a638a4efa0a29949ce9c2ff31f523
haihan-lin <haihan.lin.7@gmail.com>
2022-01-19 15:42:18 -0700

    change export

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,22 @@ import { BarChartWithDH } from './BarChart';
 import { BarChartDataPoint } from './types';
 
 let createdClassObject: BarChartWithDH;
-export function createBarChart(dataInput: BarChartDataPoint[], width: number, height: number, userName: string, previousSavedDatahunches?: string) {
-    createdClassObject = new BarChartWithDH(dataInput, width, height, userName, previousSavedDatahunches);
-    return createdClassObject.createBarChart();
-}
+// export function createBarChart(dataInput: BarChartDataPoint[], width: number, height: number, userName: string, previousSavedDatahunches?: string) {
+//     createdClassObject = new BarChartWithDH(dataInput, width, height, userName, previousSavedDatahunches);
+//     return createdClassObject.createBarChart();
+// }
 
-export function getRecordsOnBar() {
-    return createdClassObject.savedDataHunches;
-}
+// export function getRecordsOnBar() {
+//     return createdClassObject.savedDataHunches;
+// }
 
+export default {
+    createBarChart(dataInput: BarChartDataPoint[], width: number, height: number, userName: string, previousSavedDatahunches?: string) {
+        createdClassObject = new BarChartWithDH(dataInput, width, height, userName, previousSavedDatahunches);
+        return createdClassObject.createBarChart();
+    },
 
-// It can save to array
-const arrayToIncre: number[] = [];
-export function increment(input: number) {
-    arrayToIncre.push(input);
-}
-
-export function getArray() {
-    return arrayToIncre;
-}
+    getRecordsOnBar() {
+        return createdClassObject.savedDataHunches;
+    }
+};
